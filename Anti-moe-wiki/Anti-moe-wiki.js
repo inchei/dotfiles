ff65769f776137e1c69d1283ec2a9855934a0f0c
inchei <inchei@protonmail.com>
2018-02-14 16:54:04 +0800

    Fix some bugs; Make the contents material design.

--- Anti-moe-wiki/Anti-moe-wiki.js
+++ Anti-moe-wiki/Anti-moe-wiki.js
@@ -1,4 +1,5 @@
 (function($, mw) {
+    //配合vector.css显示
     var str = window.location.href;
     if (str.indexOf("Mainpage") < 0)
         $("body").prepend('<a href="https://zh.moegirl.org"><img class="topButton1" src="https://i.loli.net/2017/08/07/59886347bd66f.png"></a>');
@@ -16,13 +17,6 @@
     $("div#p-personal").prepend('<span class="topTitle" title="' + topTitle + '">' + topTitle + '</span>');
     $(".topTitle").before('<a href="https://zh.moegirl.org" style="position: absolute; left: 15%;"><img width="30px" src="https://img.moegirl.org/enmoegirl/6/6f/Logo.svg" alt=""/></a>');
 
-    $("#mw-content-text img").each(function() {
-        if (this.src == "https://static.mengniang.org/common/thumb/d/d1/%E5%A4%A7%E8%90%8C%E5%AD%97.png/50px-%E5%A4%A7%E8%90%8C%E5%AD%97.png") {
-            this.src = "https://i.loli.net/2017/08/02/5981bf14372d1.png";
-            this.srcset = "";
-        }
-    });
-
     if (document.body.scrollHeight >= $(window).height() + 300) {
         $("#p-personal").hide();
         window.onscroll = function() {
@@ -37,4 +31,27 @@
         $("#p-personal").css("box-shadow", "none");
         $("#p-personal").css("border", "none");
     }
+
+    //仅在有背景图时使主体背景半透明
+    if ($('.sidebar-character').length > 0) {
+        $('.ns-0 div#content, .ns-0 div#p-cactions li a:hover, .ns-0 div#p-cactions li.selected a, .ns-0 div#content div.thumb, div#content, div#p-cactions li a:hover, div#p-cactions li.selected a, div#content div.thumb').css('background', 'rgba(255, 255, 255, .9)');
+        $('#mw-head-base').css('box-shadow', '0 140px 0 300px rgba(93, 130, 138, .5)');
+        $('#mw-head-base').css('background', 'rgba(93, 130, 138, .5)');
+    }
+
+    //更改大萌字图源
+    $("#mw-content-text img").each(function() {
+        if (this.src == "https://static.mengniang.org/common/thumb/d/d1/%E5%A4%A7%E8%90%8C%E5%AD%97.png/50px-%E5%A4%A7%E8%90%8C%E5%AD%97.png") {
+            this.src = "https://i.loli.net/2017/08/02/5981bf14372d1.png";
+            this.srcset = "";
+        }
+    });
+
+    //暴力解决无法显示图片的问题（已修复）
+    /*$(function (){
+    $("img").each(function() {
+            this.src = this.src.replace('static', 'img');
+            this.srcset = this.srcset.replace('static', 'img');
+        });
+    });*/
 })(jQuery, mediaWiki);
