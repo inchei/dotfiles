e5289ca1f92d4e4587f67368306ed918df2e3b96
inchei <inchei@protonmail.com>
2025-11-07 21:17:02 +0800

    [+] update

--- Firefox/user.part.js
+++ Firefox/user.part.js
@@ -5,3 +5,4 @@ user_pref("font.name-list.emoji", "Noto Color Emoji, Twemoji Mozilla")
 /* Allow addons to work on addons.mozilla.org and support.mozilla.org */
 user_pref("extensions.webextensions.restrictedDomains", "accounts-static.cdn.mozilla.net,accounts.firefox.com,addons.cdn.mozilla.net,api.accounts.firefox.com,content.cdn.mozilla.net,discovery.addons.mozilla.org,install.mozilla.org,oauth.accounts.firefox.com,profile.accounts.firefox.com,sync.services.mozilla.com");
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("browser.ml.enable", false);
